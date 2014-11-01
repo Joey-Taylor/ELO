403419956ed79f1d19edb03c2f929a3c2f00bb3b
Dean Merchant <deanmerchant@gmail.com>
2014-11-01 18:37:06 +0000

    Models are no longer loaded from text files

--- src/EloWeb/Global.asax.cs
+++ src/EloWeb/Global.asax.cs
@@ -12,7 +12,6 @@ namespace EloWeb
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            RestoreModelFromTextFiles.Load(Server.MapPath(@"~\App_Data\"));
         }
     }
 }
