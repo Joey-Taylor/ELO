3bb3a7b3f05bf1554ed0ccdc23a6526c076991e7
Joey Taylor <Joey.Taylor@softwire.com>
2014-11-05 18:47:17 +0000

    Added a time to games

--- src/EloWeb.Tests.UnitTests/PlayersTests.cs
+++ src/EloWeb.Tests.UnitTests/PlayersTests.cs
@@ -107,28 +107,33 @@ namespace EloWeb.Tests.UnitTests
 
         private void InitialiseTestGames()
         {
-            Games.Initialise(new List<String>
+            var games = new Game[]
             {
-                "Peter beat Frank", 
-                "Frank beat Peter", 
-                "Frank beat Peter",
-                "Bob beat Richard",
-                "Bob beat Richard",
-                "Bob beat Richard",
-                "Richard beat Bob",
-                "Bob beat Richard",
-                "Bob beat Richard",
-                "Bob beat Richard",
-                "Richard beat Bob",
-                "Bob beat Richard",
-                "Richard beat Bob",
-                "Bob beat Richard",
-                "Bob beat Richard",
-                "Bob beat Richard",
-                "Bob beat Richard",
-                "Richard beat Bob",
-                "Bob beat Richard"
-            });
+                new Game {Winner = "Peter", Loser = "Frank"},
+                new Game {Winner = "Frank", Loser = "Peter"},
+                new Game {Winner = "Frank", Loser = "Peter"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Richard", Loser = "Bob"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Richard", Loser = "Bob"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Richard", Loser = "Bob"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Bob", Loser = "Richard"},
+                new Game {Winner = "Richard", Loser = "Bob"},
+                new Game {Winner = "Bob", Loser = "Richard"}
+            };
+
+            foreach (var game in games)
+            {
+                Games.Add(game);
+            }
         }
     }
 }
--- src/EloWeb/Models/Game.cs
+++ src/EloWeb/Models/Game.cs
@@ -5,33 +5,35 @@ namespace EloWeb.Models
     public class Game
     {
         private const string BEAT = "beat";
+        private const string AT = "at";
  
         public string Winner { get; set; }
         public string Loser { get; set; }
-        
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// Default constructor which sets the Time as DateTime.UtcNow
+        /// </summary>
+        public Game()
+        {
+            Time = DateTime.UtcNow;
+        }
+
         public static Game FromString(string game)
         {
-            var splitOn = new[] { BEAT };
-            var players = game.Split(splitOn, StringSplitOptions.None);
-            return new Game { Winner = players[0].Trim(), Loser = players[1].Trim() };
+            var splitOn = new[] { BEAT, AT };
+            var splitString = game.Split(splitOn, StringSplitOptions.None);
+            return new Game { Winner = splitString[0].Trim(), Loser = splitString[1].Trim(), Time = DateTime.Parse(splitString[2].Trim()) };
         }
 
         public override string ToString()
         {
-            return String.Format("{0} {1} {2}", Winner, BEAT ,Loser);
-        }       
+            return String.Format("{0} {1} {2} {3} {4:O}", Winner, BEAT ,Loser, AT, Time);
+        }
 
         protected bool Equals(Game other)
         {
-            return string.Equals(Winner, other.Winner) && string.Equals(Loser, other.Loser);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return ((Winner != null ? Winner.GetHashCode() : 0) * 397) ^ (Loser != null ? Loser.GetHashCode() : 0);
-            }
+            return string.Equals(Winner, other.Winner) && string.Equals(Loser, other.Loser) && Time.Equals(other.Time);
         }
 
         public override bool Equals(object obj)
@@ -40,6 +42,17 @@ namespace EloWeb.Models
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != this.GetType()) return false;
             return Equals((Game) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Winner != null ? Winner.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (Loser != null ? Loser.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ Time.GetHashCode();
+                return hashCode;
+            }
         }
     }
 }
