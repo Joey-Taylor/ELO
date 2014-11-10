e08cd93a2b178a9bb3d3c73b47fc600bf63fbee4
Joey Taylor <Joey.Taylor@softwire.com>
2014-11-10 11:35:28 +0000

    Added a graphs page

--- src/EloWeb/Controllers/GraphsController.cs
+++ src/EloWeb/Controllers/GraphsController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Web.Helpers;
 using System.Web.Mvc;
 using EloWeb.Models;
 
@@ -7,50 +6,19 @@ namespace EloWeb.Controllers
 {
     public class GraphsController : Controller
     {
-        public ActionResult PlayerEloByGames(string playerName, int? width, int? height)
+        public ActionResult Index(GraphsViewModel model)
         {
-            var player = Players.PlayerByName(playerName);
+            if (model == null || model.Names == null)
+            {
+                var names = Players.All()
+                    .OrderBy(p => p.Name)
+                    .Select(p => new CheckBoxViewModel{Checked = true, Text = p.Name})
+                    .ToList();
 
-            var ratings = player.Ratings.OrderBy(r => r.TimeFrom);
+                model = new GraphsViewModel {Names = names, GraphType = GraphType.EloVsGames};
+            }
 
-            var yValues = ratings.Select(r => r.Value).ToArray();
-            var xValues = ratings.Select((x, i) => i).ToArray();
-
-            new Chart(width: width ?? 500, height: height ?? 400)
-                .AddSeries(
-                    name: "ELO",
-                    xValue: xValues,
-                    yValues: yValues,
-                    chartType: "Line")
-                .AddTitle("ELO vs Games")
-                .SetXAxis("Games", xValues.Min(), xValues.Max())
-                .SetYAxis("ELO", yValues.Min()-100, yValues.Max()+100)
-                .Write("png");
-
-            return new EmptyResult();
-        }
-
-        public ActionResult PlayerEloByTime(string playerName, int? width, int? height)
-        {
-            var player = Players.PlayerByName(playerName);
-
-            var ratings = player.Ratings.OrderBy(r => r.TimeFrom);
-
-            var yValues = ratings.Select(r => r.Value).ToArray();
-            var xValues = ratings.Select(r => r.TimeFrom).ToArray();
-
-            new Chart(width: width ?? 500, height: height ?? 400)
-                .AddSeries(
-                    name: "ELO",
-                    xValue: xValues,
-                    yValues: yValues,
-                    chartType: "Line")
-                .AddTitle("ELO vs Time")
-                .SetXAxis("Time", xValues.Min().ToOADate(), xValues.Max().ToOADate())
-                .SetYAxis("ELO", yValues.Min() - 100, yValues.Max() + 100)
-                .Write("png");
-
-            return new EmptyResult();
+            return View(model);
         }
 	}
 }
---
+++ src/EloWeb/Models/GraphsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace EloWeb.Models
+{
+    public class GraphsViewModel
+    {
+        public List<CheckBoxViewModel> Names { get; set; }
+        public GraphType GraphType { get; set; }
+    }
+
+    public enum GraphType
+    {
+        [Display(Name = "ELO vs Games")]
+        [Description("SelectedEloByGames")]
+        EloVsGames,
+
+        [Display(Name = "ELO vs Time")]
+        [Description("SelectedEloByTime")]
+        EloVsTime
+    }
+
+    public static class GraphTypeExtensions
+    {
+        public static string ToDisplayString(this GraphType enumValue)
+        {
+            var display = enumValue.GetType()
+                           .GetMember(enumValue.ToString()).First()
+                           .GetCustomAttributes(false)
+                           .OfType<DisplayAttribute>()
+                           .LastOrDefault();
+
+            return display != null ? display.GetName() : enumValue.ToString();
+        }
+
+        public static string ToActionName(this GraphType enumValue)
+        {
+            var description = enumValue.GetType()
+                           .GetMember(enumValue.ToString()).First()
+                           .GetCustomAttributes(false)
+                           .OfType<DescriptionAttribute>()
+                           .Last();
+
+            return description.Description;
+        }
+    }
+}
